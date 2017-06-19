f635109508a621f2d31dcc252335842784aec6c6
Keith Mattix II <keithmattix2@gmail.com>
2017-06-19 11:02:04 -0500

    Add tests for dynamic associations on surveys

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,15 +11,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130201105206) do
+ActiveRecord::Schema.define(:version => 20170619155608) do
+
+  create_table "lessons", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "survey_answers", :force => true do |t|
     t.integer  "attempt_id"
     t.integer  "question_id"
     t.integer  "option_id"
     t.boolean  "correct"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "option_text"
+    t.integer  "option_number"
+    t.integer  "predefined_value_id"
   end
 
   create_table "survey_attempts", :force => true do |t|
@@ -31,34 +40,68 @@ ActiveRecord::Schema.define(:version => 20130201105206) do
 
   create_table "survey_options", :force => true do |t|
     t.integer  "question_id"
-    t.integer  "weight",      :default => 0
+    t.integer  "weight",          :default => 0
     t.string   "text"
     t.boolean  "correct"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "locale_text"
+    t.integer  "options_type_id"
+    t.string   "head_number"
+  end
+
+  create_table "survey_predefined_values", :force => true do |t|
+    t.string   "head_number"
+    t.string   "name"
+    t.string   "locale_name"
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "survey_questions", :force => true do |t|
-    t.integer  "survey_id"
     t.string   "text"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "section_id"
+    t.string   "head_number"
+    t.text     "description"
+    t.string   "locale_text"
+    t.string   "locale_head_number"
+    t.text     "locale_description"
+    t.integer  "questions_type_id"
+    t.boolean  "mandatory",          :default => false
+  end
+
+  create_table "survey_sections", :force => true do |t|
+    t.string   "head_number"
+    t.string   "name"
+    t.text     "description"
+    t.integer  "survey_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "locale_head_number"
+    t.string   "locale_name"
+    t.text     "locale_description"
   end
 
   create_table "survey_surveys", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.integer  "attempts_number", :default => 0
-    t.boolean  "finished",        :default => false
-    t.boolean  "active",          :default => false
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
+    t.integer  "attempts_number",    :default => 0
+    t.boolean  "finished",           :default => false
+    t.boolean  "active",             :default => true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "locale_name"
+    t.text     "locale_description"
+    t.integer  "lesson_id"
   end
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -1,12 +1,11 @@
 class SurveyTest < ActiveSupport::TestCase
-
-  test "should not create a valid survey without sections" do
+  test 'should not create a valid survey without sections' do
     survey = create_survey
     should_not_be_persisted survey
   end
 
-  test "should not create a  survey with active flag true and empty questions collection" do
-    surveyA = create_survey({:active => true})
+  test 'should not create a  survey with active flag true and empty questions collection' do
+    surveyA = create_survey(active: true)
     surveyB = create_survey_with_sections(2)
     surveyB.active = true
     surveyB.save
@@ -16,32 +15,39 @@ class SurveyTest < ActiveSupport::TestCase
     should_be_true surveyB.valid?
   end
 
-  test "should create a survey with 3 sections" do
+  test 'should create a survey with 3 sections' do
     num_questions = 3
     survey = create_survey_with_sections(num_questions, num_questions)
     should_be_persisted survey
     assert_equal survey.sections.size, num_questions
   end
-  
-  test "should create a survey with 2 questions" do
+
+  test 'should create a survey with 2 questions' do
     num_questions = 2
     survey = create_survey_with_sections(num_questions, 1)
     should_be_persisted survey
     assert_equal survey.sections.first.questions.size, num_questions
   end
 
-  test "should not create a survey with attempts_number lower than 0" do
-    survey = create_survey({:attempts_number => -1})
+  test 'should not create a survey with attempts_number lower than 0' do
+    survey = create_survey(attempts_number: -1)
     should_not_be_persisted survey
   end
 
-  test "should not save survey without all the needed fields" do
-    survey_without_name = create_survey({:name => nil})
-    survey_without_description = create_survey({:description => nil})
+  test 'should not save survey without all the needed fields' do
+    survey_without_name = create_survey(name: nil)
+    survey_without_description = create_survey(description: nil)
     %w(name description).each do |suffix|
       should_not_be_persisted eval("survey_without_#{suffix}")
     end
   end
 
+  test 'should have the correct associations via "has_many_surveys"' do
+    lesson = create_lesson
+    survey = create_survey_with_sections(2, 1)
+    survey.lesson_id = lesson.id
+    survey.save
 
+    assert_equal survey, lesson.surveys.first
+  end
 end
