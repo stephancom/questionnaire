2636b16d3ff7ec96da41ad35d288f31d70d17a98
Keith Mattix II <keithmattix2@gmail.com>
2017-06-20 15:37:13 -0500

    Add the free response question type

--- app/models/survey/questions_type.rb
+++ app/models/survey/questions_type.rb
@@ -1,27 +1,27 @@
 class Survey::QuestionsType
-  @@questions_types = {:general => 1}
-  
+  @@questions_types = { general: 1, free_response: 2 }
+
   def self.questions_types
     @@questions_types
   end
-  
+
   def self.questions_types_title
     titled = {}
-    Survey::QuestionsType.questions_types.each{|k, v| titled[k.to_s.titleize] = v}
+    Survey::QuestionsType.questions_types.each { |k, v| titled[k.to_s.titleize] = v }
     titled
   end
-  
+
   def self.questions_type_ids
     @@questions_types.values
   end
-  
+
   def self.questions_type_keys
     @@questions_types.keys
   end
-  
+
   @@questions_types.each do |key, val|
-    define_singleton_method "#{key}" do
-        val
+    define_singleton_method key.to_s do
+      val
     end
   end
 end
