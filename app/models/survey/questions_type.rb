4e5f1f86b895485011a3edece19a8f64fa7b522b
Keith Mattix II <keithmattix2@gmail.com>
2017-06-21 11:02:38 -0500

    Just standardize the question types to kind of match the option types

--- app/models/survey/questions_type.rb
+++ app/models/survey/questions_type.rb
@@ -1,5 +1,5 @@
 class Survey::QuestionsType
-  @@questions_types = { general: 1, free_response: 2 }
+  @@questions_types = { multiple_choice: 1, free_response: 2, multi_select: 3 }
 
   def self.questions_types
     @@questions_types
